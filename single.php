eb27b8da7089b3fecf17678617c4ca0662658a0d
hannaliebl <hanna@hannaliebl.com>
2013-10-15 11:59:15 -0700

    single post page styling

--- single.php
+++ single.php
@@ -1,65 +1,39 @@
 <?php get_header(); ?>
-	
-	<!-- section -->
-	<section role="main">
+		<section class="content container" id="intro" role="main">
+            <div class="container960">
 	
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-			
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-			
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-			
-			<?php the_content(); // Dynamic Content ?>
-			
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-			
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-			
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-			
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+		 <article class="blog-entry" id="post-<?php the_ID(); ?>">
+        <header>
+          <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+            <h1><?php the_title(); ?></h1>
+          </a>
+          <span class="meta"><?php the_time('F j, Y') ?> Posted in: <?php the_category(', '); ?>, <?php the_tags('Tagged: ', ', ', ''); ?> by <?php the_author() ?></span> 
+        </header>
+        <?php the_content('Read the rest of this entry &raquo;'); ?>
+        <hr>
+          <span class="comments-link">
+            <a href="<?php comments_link(); ?>"><?php comments_number( '0 Comments', '1 Comment', '% Comments'); ?></a> | <a href="<?php the_permalink() ?>#respond">Leave a Comment</a>
+            <?php edit_post_link('Edit Post', ' | ' ); ?>
+          </span>
 			
 			<?php comments_template(); ?>
 			
 		</article>
-		<!-- /article -->
 		
 	<?php endwhile; ?>
 	
 	<?php else: ?>
 	
-		<!-- article -->
 		<article>
 			
 			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 			
 		</article>
-		<!-- /article -->
 	
 	<?php endif; ?>
-	
+	</div>
 	</section>
-	<!-- /section -->
-	
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
